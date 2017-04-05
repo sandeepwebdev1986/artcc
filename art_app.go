741de140e0ff44914a2b6f76226439b78e1afab5
Sandeep Patel <sandeep.patel@alepo.com>
2017-04-05 09:29:29 +0530

    ASD

--- art_app.go
+++ art_app.go
@@ -50,6 +50,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"itpUtils"
 	// "github.com/errorpkg"
 )
 
@@ -272,6 +273,28 @@ func QueryFunction(fname string) func(stub shim.ChaincodeStubInterface, function
 	return QueryFunc[fname]
 }
 
+var PictureMap = map[string]string {
+	"art1.png":        "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/art1.png",
+	"art2.png":        "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/art2.png",
+	"art3.png":        "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/art3.png",
+	"art4.png":        "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/art4.png",
+	"art5.png":        "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/art5.png",
+	"art6.png":        "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/art6.png",
+	"art7.png":        "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/art7.png",
+	"item-001.jpg":    "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/item-001.jpg",
+	"item-002.jpg":    "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/item-002.jpg",
+	"item-003.jpg":    "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/item-003.jpg",
+	"item-004.jpg":    "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/item-004.jpg",
+	"item-005.jpg":    "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/item-005.jpg",
+	"item-006.jpg":    "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/item-006.jpg",
+	"item-007.jpg":    "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/item-007.jpg",
+	"item-008.jpg":    "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/item-008.jpg",
+	"people.gif":      "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/people.gif",
+	"mad-fb.jpg":      "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/mad-fb.gif",
+	"sample.png":      "https://raw.githubusercontent.com/ITPeople-Blockchain/auction/v0.6/art/artchaincode/sample.png",
+}
+
+
 //var myLogger = logging.MustGetLogger("auction_trading")
 
 type SimpleChaincode struct {
@@ -279,6 +302,32 @@ type SimpleChaincode struct {
 
 var gopath string
 var ccPath string
+
+func downloadFile(filepath string, url string) (err error) {
+
+  // Create the file
+  out, err := os.Create(filepath)
+  if err != nil  {
+    return err
+  }
+  defer out.Close()
+
+  // Get the data
+  resp, err := http.Get(url)
+  if err != nil {
+    return err
+  }
+  defer resp.Body.Close()
+
+  // Writer the body to file
+  _, err = io.Copy(out, resp.Body)
+  if err != nil  {
+    return err
+  }
+
+  return nil
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // Chain Code Kick-off Main function
@@ -335,6 +384,15 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	err = stub.PutState("version", []byte(strconv.Itoa(23)))
 	if err != nil {
 		return nil, err
+	}
+
+	for k, v := range PictureMap {
+    		fmt.Printf("\n Downloading Image '%s' from URL:  %s", k, v)
+	        err = downloadFile(k, v)
+	        if err != nil {
+	        	fmt.Println(err)
+			return shim.Error("Invoke: Invalid Function Name - function names begin with a q or i")
+		}
 	}
 
 	fmt.Println("Init() Initialization Complete  : ", args)
